684017769d0d969a218bc92f192a9198738c464a
Mike Jones <mike@westerncapelabs.com>
2015-07-15 22:35:55 +0200

    Remove stray print

--- pystrich/fonts/__init__.py
+++ pystrich/fonts/__init__.py
@@ -6,6 +6,5 @@ from PIL import ImageFont
 def get_font(font_name, font_size):
     fontdir = os.path.dirname(os.path.abspath(__file__))
     fontfile = os.path.join(fontdir, "%s%02d.pil" % (font_name, font_size))
-    print(fontfile)
     font = ImageFont.load_path(fontfile)
     return font
